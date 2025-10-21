6f8f7e4d97d5caa101f0b04c771cce7ab9b4afec
tyrion21 <jhermidadiaz@gmail.com>
2025-10-21 00:53:45 -0300

    fix: update API_URL to use the correct endpoint and comment out unused URLs

--- constants/constantes.js
+++ constants/constantes.js
@@ -1,7 +1,7 @@
 //export const API_URL = 'http://172.16.205.95:4000'
-//export const API_URL = 'https://192.168.7.26:4000'
+export const API_URL = 'https://192.168.7.26:4000'
 // export const API_URL = 'http://10.0.2.2:4000'
-export const API_URL = 'http://192.168.1.4:3000'
+//export const API_URL = 'http://192.168.1.4:3000'
 
 // Constante de temporada - cambiar aquí cuando cambie la temporada
 export const TEMPORADA = '9'
